d94e4a475d41735bec72fa98b835d7b22988c57b
syhliu-dev <53819559+syhliu-dev@users.noreply.github.com>
2019-08-21 13:21:04 +0800

    Update housing.py

--- housing.py
+++ housing.py
@@ -14,9 +14,6 @@ import pandas as pd
 import numpy as np
 from scipy.stats import ttest_ind
 
-
-# # Assignment 4 - Hypothesis Testing
-# This assignment requires more individual learning than previous assignments - you are encouraged to check out the [pandas documentation](http://pandas.pydata.org/pandas-docs/stable/) to find functions or methods you might not have used yet, or ask questions on [Stack Overflow](http://stackoverflow.com/) and tag them as pandas and python related. And of course, the discussion forums are open for interaction with your peers and the course staff.
 # 
 # Definitions:
 # * A _quarter_ is a specific three month period, Q1 is January through March, Q2 is April through June, Q3 is July through September, Q4 is October through December.
@@ -31,7 +28,6 @@ from scipy.stats import ttest_ind
 # * From the Wikipedia page on college towns is a list of [university towns in the United States](https://en.wikipedia.org/wiki/List_of_college_towns#College_towns_in_the_United_States) which has been copy and pasted into the file ```university_towns.txt```.
 # * From Bureau of Economic Analysis, US Department of Commerce, the [GDP over time](http://www.bea.gov/national/index.htm#gdp) of the United States in current dollars (use the chained value in 2009 dollars), in quarterly intervals, in the file ```gdplev.xls```. For this assignment, only look at GDP data from the first quarter of 2000 onward.
 # 
-# Each function in this assignment below is worth 10%, with the exception of ```run_ttest()```, which is worth 50%.
 
 # In[6]:
 
